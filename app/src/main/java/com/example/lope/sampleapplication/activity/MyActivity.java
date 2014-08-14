c67b942af21ec3ccd92e8dea0c0d35d5b4b579f4
Lope Emano <lopegwapo@gmail.com>
2014-08-14 21:54:39 +0800

    updated file

--- app/src/main/java/com/example/lope/sampleapplication/activity/MyActivity.java
+++ app/src/main/java/com/example/lope/sampleapplication/activity/MyActivity.java
@@ -1,35 +1,38 @@
-package com.metricbpo.myapplication2.activity;
+package com.example.lope.sampleapplication.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.metricbpo.myapplication2.app.R;
-import com.metricbpo.myapplication2.task.RequestTask;
+import com.example.lope.sampleapplication.R;
 
-/**
- * a background thread cannot access the main thread
- * http://developer.android.com/guide/components/processes-and-threads.html
- * <p/>
- * how to make it better
- */
-public class MainActivity extends Activity {
+
+public class MyActivity extends Activity {
+
+    //global class variable
+    EditText edtQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_my);
+
         //findViewById is accessible within the Activity class
         //(Button) is called type casting. findViewById returns a View object so we need to cast it to a Button object
         //this is why btnLogin (btn) naming convention is important
         Button button = (Button) findViewById(R.id.btnSubmit);
+        edtQuestion = (EditText) findViewById(R.id.edtQuestion);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String message = edtQuestion.getText().toString();
                 // Let's use the application's context for example purposes
-                Toast.makeText(getApplicationContext(), "You clicked me!", Toast.LENGTH_SHORT);
+                Toast.makeText(getApplicationContext(), "You clicked me!" + message, Toast.LENGTH_SHORT).show();
             }
         });
     }
